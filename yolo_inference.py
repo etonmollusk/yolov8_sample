c05fc051c63e8a915c72a50760491fe31d70829e
Elton Shon <elton@industrialnext.ai>
2023-11-28 13:09:17 -0800

    add yolo_inference.py

---
+++ yolo_inference.py
@@ -0,0 +1,51 @@
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
+
+import cv2
+import urllib.request
+import numpy as np
+import torch
+
+
+test_img_url = "https://ultralytics.com/images/bus.jpg"
+yolo_model = "yolov8n-pose.pt"
+
+
+def load_test_img_url(url):
+    req = urllib.request.urlopen(url)
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    
+    return cv2.imdecode(arr, -1)
+
+
+def get_model():
+    # Load a model
+    model = YOLO(yolo_model)
+    
+    # Move model to gpu
+    model.to("cuda")
+
+    return model
+
+if __name__ == "__main__":
+    img = load_test_img_url(test_img_url)
+    model = get_model()
+
+    # Predict with the model
+    results = model(img)
+    
+    annotator = Annotator(img)
+    
+    # View results
+    for r in results:
+        for i in range(r.keypoints.shape[0]):
+            conf = torch.unsqueeze(r.keypoints.conf[i], dim=1)
+            ktps = torch.cat((r.keypoints.xy[i], conf), 1)
+            annotator.kpts(ktps, shape=r.keypoints.orig_shape, kpt_line=True)
+    
+    img = annotator.result()  
+    cv2.imshow('yolov8_detection', img)     
+    if cv2.waitKey() & 0xFF == 27:
+        pass
+
+
